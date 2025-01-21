8c42016d8a314c545de61d5cd09729c2d30e8c4a
Sarun Daunghirun <125266560+sdsarun@users.noreply.github.com>
2025-01-21 13:58:10 +0700

    fix(utils-validation): mismatch condition throw error on validate dto

--- src/shared/utils/validation/dto.validation.ts
+++ src/shared/utils/validation/dto.validation.ts
@@ -1,18 +1,20 @@
-import { validate } from 'class-validator';
+import { validate, ValidationError } from 'class-validator';
 import { ServiceActionOptions } from 'src/shared/types/service-action';
 
 export async function validateDTO<T extends object>(
   dto: T,
   options?: ServiceActionOptions,
-): Promise<void> {
+): Promise<ValidationError[]> {
   const errors = await validate(dto, { whitelist: true });
 
   if (errors.length) {
-    if (options?.throwErrorOnValidateFailed) {
-      if (typeof options?.onError === 'function') {
-        options?.onError(errors);
-      } else {
+    if (typeof options?.onError === 'function') {
+      options?.onError(errors);
+    } else {
+      if (options?.throwErrorOnValidateFailed) {
         throw new Error(errors.toString());
+      } else {
+        return errors;
       }
     }
   }
