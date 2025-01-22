b81c6032fc2ce775d1aa0d02417fa4b9fd1693b9
Sarun Daunghirun <sdsarun@outlook.com>
2025-01-22 23:44:19 +0700

    feat(core): add skip format interceptor

---
+++ src/common/decorators/skip-format-interceptor.decorator.ts
@@ -0,0 +1,11 @@
+export const SkipFormatResponseInterceptorPropertyName = Symbol('SkipFormatResponseInterceptorPropertyName');
+
+export function SkipFormatResponseInterceptor() {
+  return function (
+    _target: any,
+    _propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    descriptor.value[SkipFormatResponseInterceptorPropertyName] = true;
+  };
+}
--- src/common/interceptors/format-response.interceptor.ts
+++ src/common/interceptors/format-response.interceptor.ts
@@ -6,19 +6,23 @@ import {
 } from '@nestjs/common';
 import { Response } from 'express';
 import { map, Observable } from 'rxjs';
-import { Logger } from 'src/logger/logger.service';
+import { SkipFormatResponseInterceptorPropertyName } from '../decorators/skip-format-interceptor.decorator';
 
 @Injectable()
 export class FormatResponseInterceptor implements NestInterceptor {
-  constructor(private readonly logger: Logger) {}
+  constructor() {}
 
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    this.logger.setContext(FormatResponseInterceptor.name);
+    const isSkip = context.getHandler()[SkipFormatResponseInterceptorPropertyName];
+    if (isSkip) {
+      return next.handle();
+    }
 
     const response = context.switchToHttp().getResponse<Response>();
+
     return next.handle().pipe(
       map((controllerResult) => {
         const message = controllerResult?.message ?? 'Success';
