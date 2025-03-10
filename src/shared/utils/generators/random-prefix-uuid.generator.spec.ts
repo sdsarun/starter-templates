f084fef4ad17f6169e873279432bcb9e832f7300
Sarun Daunghirun <sdsarun@outlook.com>
2025-03-10 22:39:40 +0700

    test(utils): add test case

--- src/shared/utils/generators/random-prefix-uuid.generator.spec.ts
+++ src/shared/utils/generators/random-prefix-uuid.generator.spec.ts
@@ -7,14 +7,22 @@ jest.mock('node:crypto', () => ({
 
 describe('randomPrefixUUID', () => {
   it('should concatenate prefix with a UUID without dashes', () => {
-    const prefix = 'request-auto-gen';
+    let prefix = 'request-auto-gen';
     const mockUUID = '123e4567-e89b-12d3-a456-426614174000';
     (randomUUID as jest.Mock).mockReturnValue(mockUUID);
 
-    const result = randomPrefixUUID(prefix);
+    let result = randomPrefixUUID(prefix);
 
     expect(randomUUID).toHaveBeenCalled();
     expect(result).toEqual('request-auto-gen_123e4567e89b12d3a456426614174000');
+
+    prefix = "guest-user"
+    result = randomPrefixUUID(prefix)
+    expect(result).toEqual(`guest-user_123e4567e89b12d3a456426614174000`);
+
+    prefix = "guest_user"
+    result = randomPrefixUUID(prefix)
+    expect(result).toEqual(`guest_user_123e4567e89b12d3a456426614174000`);
   });
 
   it('should handle an empty prefix', () => {
